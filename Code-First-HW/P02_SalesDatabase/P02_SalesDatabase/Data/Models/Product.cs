4c9fb421cfc67cb68462013008f07ec009441028
Rumen Novachkov <r.novachkov@gmail.com>
2017-11-11 08:13:47 +0200

    Drd
    rdr

---
+++ Code-First-HW/P02_SalesDatabase/P02_SalesDatabase/Data/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace P03_SalesDatabase.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class Product
+    {
+        public Product()
+        {
+            Sale = new List<Sale>();
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Sale> Sale { get; set; }
+    }
+}
