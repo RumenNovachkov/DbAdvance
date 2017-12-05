e3ea88726e1b414a6f01c39a6c523ddf94fd5c1a
Rumen Novachkov <r.novachkov@gmail.com>
2017-12-05 21:43:25 +0200

    Update Invitation Ignore

--- TeamBuilder/TeamBuilder.Data/Configurations/UserConfig.cs
+++ TeamBuilder/TeamBuilder.Data/Configurations/UserConfig.cs
@@ -9,7 +9,25 @@
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-			builder.Ignore(e => e.Invitation);
-        }
+ -            builder.HasKey(e => e.Id);
+ -
+ -            builder.HasAlternateKey(e => e.Username);
+ -
+ -            builder.Property(e => e.Username)
+ -                .IsRequired()
+ -                .HasMaxLength(25);
+ -
+ -            builder.Property(e => e.FirstName)
+ -                .IsRequired()
+ -                .HasMaxLength(25);
+ -
+ -            builder.Property(e => e.LastName)
+ -                .IsRequired()
+ -                .HasMaxLength(25);
+ -
+ -            builder.Property(e => e.Password);
+ 
+ 	      builder.Ignore(e => e.Invitation);
+	}
     }
 }
