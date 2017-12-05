0b3beb9bee12109e9acbd7f2f7236e63bfbd72fe
Rumen Novachkov <r.novachkov@gmail.com>
2017-12-05 21:44:42 +0200

    Add Invitation Ignore

--- TeamBuilder/TeamBuilder.Data/Configurations/TeamConfig.cs
+++ TeamBuilder/TeamBuilder.Data/Configurations/TeamConfig.cs
@@ -9,7 +9,17 @@
     {
         public void Configure(EntityTypeBuilder<Team> builder)
         {
+ +			builder.HasKey(e => e.Id);
+ +			
+ +			builder.Property(e => e.Name).IsRequired().IsUnicode().HasMaxLength(25);
+ +
+ +			builder.Property(e => e.Description).IsUnicode().HasMaxLength(250);
+ +
+ +			builder.Property(e => e.StartDate).HasColumnType("DATETIME2");// {dd/MM/yyyy HH:mm} How?
+ +			builder.Property(e => e.EndDate).HasColumnType("DATETIME2");//{dd/MM/yyyy HH:mm} Must be after StartDate
+ +			builder.HasOne(e => e.Creator).WithMany(e => e.Events).HasForeignKey(e => e.CreatorId);
+ 
 			builder.Ignore(e => e.Invitation);
-		}
+	}
     }
 }
